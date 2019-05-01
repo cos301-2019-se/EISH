aa3b14dc0848c6c550006bb72a02922604b7f4f8
Given-Rakgoale <u15135421@tuks.co.za>
2019-05-01 13:43:35 +0200

    Add files via upload

---
+++ Backend/eishms/src/main/java/com/monotoneid/eishms/EISHMSApplication.java
@@ -0,0 +1,13 @@
+package com.monotoneid.eishms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EISHMSApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EISHMSApplication.class, args);
+	}
+
+}
