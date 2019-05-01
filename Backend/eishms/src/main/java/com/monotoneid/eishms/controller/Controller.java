24ffa2a016dcf0cf116473cee2f9bacf73bdf03c
u15321861 <koketsomolawa2@gmail.com>
2019-05-01 21:29:47 +0200

    new files

--- Backend/eishms/src/main/java/com/monotoneid/eishms/controller/Controller.java
+++ Backend/eishms/src/main/java/com/monotoneid/eishms/controller/Controller.java
@@ -1,15 +1,34 @@
 package com.monotoneid.eishms.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.monotoneid.eishms.model.DeviceRequestBody;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequestMapping("api")
 @RestController
 public class Controller {
-    
-    @GetMapping("/devices")
-    public String doSomething() {
-        return "";
+    @Autowired
+    ObjectMapper mapper;
+
+    @PostMapping("/devices")
+    public ObjectNode postRequest(@RequestBody DeviceRequestBody drb) {
+        ArrayNode aa = mapper.createArrayNode();
+        aa.add(5);
+        aa.add(6);
+        aa.add(7);
+        ObjectNode objectNode = mapper.createObjectNode();
+        if (drb.getOption().matches("add_device")) {
+            objectNode.put("success", true);
+            objectNode.put("data", aa);
+        }
+        return objectNode;
     }
 }
---
+++ Backend/eishms/src/main/java/com/monotoneid/eishms/model/DeviceRequestBody.java
@@ -0,0 +1,57 @@
+package com.monotoneid.eishms.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class DeviceRequestBody {
+    private String option;
+    private String device_name;
+    private String publish_topic;
+    private String subscribe_topic;
+    private int max_watt;
+    private int min_watt;
+    private String device_type;
+    private boolean device_state;
+    private long start_date;
+    private long end_date;
+
+    public DeviceRequestBody(@JsonProperty("option") String o, @JsonProperty("device_name") String dn,
+                            @JsonProperty("publish_topic") String pt, @JsonProperty("subscribe_topic") String st,
+                            @JsonProperty("max_watt") int max, @JsonProperty("min_watt") int min,
+                            @JsonProperty("device_type") String dt) {
+        this.option = o;
+        this.device_name = dn;
+        this.publish_topic = pt;
+        this.subscribe_topic = st;
+        this.max_watt = max;
+        this.min_watt = min;
+        this.device_type = dt;                        
+    }
+
+    public String getOption() {
+        return this.option;
+    }
+
+    public String getDeviceName() {
+        return this.device_name;
+    }
+
+    public String getPublishTopic() {
+        return this.publish_topic;
+    }
+
+    public String getSubscribeTopic() {
+        return this.subscribe_topic;
+    }
+
+    public int getMaxWatt() {
+        return this.max_watt;
+    }
+
+    public int getMinWatt() {
+        return this.min_watt;
+    }
+
+    public String getDeviceType() {
+        return this.device_type;
+    }
+}
