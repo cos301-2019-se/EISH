aaf8d2d44b84d3bcae317ac3e09ec1b6a1b3d4f9
Given <u15135421@tuks.co.za>
2019-06-18 21:38:51 +0200

    New Initializer

--- Backend_Old/eishms/src/main/java/com/monotoneid/eishms/EishmsApplication.java
+++ Backend_Old/eishms/src/main/java/com/monotoneid/eishms/EishmsApplication.java
@@ -1,13 +1,14 @@
 package com.monotoneid.eishms;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class EISHMSApplication {
+public class EishmsApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EISHMSApplication.class, args);
+		SpringApplication.run(EishmsApplication.class, args);
 	}
 
 }
