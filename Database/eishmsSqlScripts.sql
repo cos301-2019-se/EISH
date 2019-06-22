396b270b651125d66bce26f1da50af35c1b6ef83
KearabiloeNare <u10068083@tuks.co.za>
2019-06-22 14:45:50 +0200

    chenged to homeuser

--- Database/eishmsSqlScripts.sql
+++ Database/eishmsSqlScripts.sql
@@ -1,16 +1,16 @@
- CREATE TYPE userType AS ENUM ('ADMIN', 'RESIDENT', 'GUEST');
+ CREATE TYPE userType AS ENUM ('ROLE_ADMIN', 'ROLE_RESIDENT', 'ROLE_GUEST');
 
- create table User(
- userid serial primary key,
+ create table homeuser(
+ userid serial  primary key,
  username text not null,
  useremail text not null,
  userpassword text not null,
  userlocationtopic text not null,
  usertype userType not null,
- userexpirydate TIMESTAMP
+ userexpirydate TIMESTAMP not null
  );
 
- insert into User(username,useremail,userpassword,userlocationtopic,usertype,userexpirydate) values('admin','admin@eishms.io','admin','owntracks/admin/iPhone/house','ROLE_ADMIN','2050-01-31 00:00:00');
-select * from User;
+ insert into homeuser(username,useremail,userpassword,userlocationtopic,usertype,userexpirydate) values('admin','admin@eishms.io','admin','owntracks/admin/iPhone/house','ROLE_ADMIN','2050-01-31 00:00:00');
+select * from homeuser;
 
 
