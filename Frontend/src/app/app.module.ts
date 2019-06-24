77df6f4c81afbdf6309a72459dbbe1a3375f5785
Fulela <u16343825@tuks.co.za>
2019-06-24 15:20:03 +0200

    partial login

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { ReactiveFormsModule} from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomepageComponent } from './pages/homepage/homepage.component';
@@ -26,6 +26,7 @@ import { HTTP_INTERCEPTORS } from '@angular/common/http';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    ReactiveFormsModule,
     HttpClientModule
   ],
   providers: [UserAccessControlService, InputService, {
--- Frontend/src/app/templates/forms/login/login.component.ts
+++ Frontend/src/app/templates/forms/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { User } from './user.ts';
+import {accessControl} from 'src/app/services/user/user-access-control.service';
 
 @Component({
   selector: 'app-login',
@@ -10,9 +13,24 @@ export class LoginComponent implements OnInit {
    * Class is responsible for loggin into the  system
    * Route to key, change credentials or homepage
    */
-  constructor() { }
+  formHeading: String;
+  loginForm: FormGroup;
+  user = new User();
+  userType: Array<string> = [
+    'admin',
+    'guest',
+    'resident'
+  ];
+  constructor(private fb: FormBuilder) {
+    this.loginForm = this.fb.group({
+      'userEmail':['',[Validators.required,Validators.EmailValidator]],
+      'userPassword':['',[Validators.required,Validators.minLength(8),Validators.maxLength(40)]]
+    });
+   }
 
   ngOnInit() {
+    this.formHeading="Login";
+    //do you call constructor here?
   }
 
   /**
@@ -20,9 +38,21 @@ export class LoginComponent implements OnInit {
    * if default admin credentials routes to changeCredentials
    * if guest has expired, route to keyPage
    */
-  login(){
+  login(formData){
     //sanitize and validate
     //if admin admin: go to server, according to response:  make token and  route to change credntials or wrong credntials
+    const regform = formData;
+    //send username and password
+    if(formData.userName === "admin")
+    {
+      ///handle response from authenticateUser()
+      //if success route to changeCredentials else reload login with error
+    }
+    else{
+      //handle response from authenticateUSer()
+      //if success route to system
+    }
+  
   }
  
 
