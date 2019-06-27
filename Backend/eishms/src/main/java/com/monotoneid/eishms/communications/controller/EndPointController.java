58d699b6fc64e50771c39d0a2f417e302f8287ab
Given <u15135421@tuks.co.za>
2019-06-27 14:56:07 +0200

    Organised imports

--- Backend/eishms/src/main/java/com/monotoneid/eishms/communications/controller/EndPointController.java
+++ Backend/eishms/src/main/java/com/monotoneid/eishms/communications/controller/EndPointController.java
@@ -8,10 +8,10 @@ import com.monotoneid.eishms.dataPersistence.models.HomeUser;
 import com.monotoneid.eishms.dataPersistence.repositories.HomeKeys;
 import com.monotoneid.eishms.services.databaseManagementSystem.UserService;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -21,7 +21,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 
 //@CrossOrigin(origins = "http://localhost:4200") 
