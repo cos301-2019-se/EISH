556c5af222212c46fff65a45afb19b6493ab7157
KearabiloeNare <u10068083@tuks.co.za>
2019-07-01 16:55:18 +0200

    added endpoints for remove device

--- Backend/eishms/src/main/java/com/monotoneid/eishms/communications/controller/DevicesEndPointController.java
+++ Backend/eishms/src/main/java/com/monotoneid/eishms/communications/controller/DevicesEndPointController.java
@@ -11,7 +11,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -45,6 +47,17 @@ public class DevicesEndPointController{
        System.out.println("RETRIEVE USER ENDPOINT!!!");
        return deviceService.retrieveDevice(deviceId);
     }
+    /**
+    * DELETE METHOD
+    * Implements removeDevice endpoint, that calls the removeDevice service
+    * @return an object with deleted device
+    */
+   @DeleteMapping("/device")
+   @PreAuthorize("hasRole('ADMIN')")
+   public ResponseEntity<Object> removeDevice(@Valid @RequestBody Device deviceToDelete){
+      return deviceService.removeDevice(deviceToDelete);
+   }
+   
 
 
 }
