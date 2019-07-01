4f4b8a786bccb9ee13f8d2a19cd4cd4e0020900a
KearabiloeNare <u10068083@tuks.co.za>
2019-07-01 16:10:04 +0200

    get by user id endpoint

--- Backend/eishms/src/main/java/com/monotoneid/eishms/services/databaseManagementSystem/DeviceService.java
+++ Backend/eishms/src/main/java/com/monotoneid/eishms/services/databaseManagementSystem/DeviceService.java
@@ -4,8 +4,11 @@ import java.util.List;
 
 import com.monotoneid.eishms.dataPersistence.models.Device;
 import com.monotoneid.eishms.dataPersistence.repositories.Devices;
+import com.monotoneid.eishms.exceptions.ResourceNotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -17,5 +20,13 @@ public class DeviceService {
     public List<Device> retrieveAllDevices(){
         return devicesRepository.findAll();
     }
-    
+    public ResponseEntity<Device> retrieveDevice(long deviceId){
+        try {
+            Device foundDevice = devicesRepository.findById(deviceId).orElseThrow(() -> new ResourceNotFoundException("Device does not exist"));
+            return new ResponseEntity<>(foundDevice,HttpStatus.OK);
+        } catch(Exception e){
+            System.out.println("Error: " + e.getMessage()+"!");
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
 }
