9e4bff384b570ed03a7eca02256330584c950147
KearabiloeNare <u10068083@tuks.co.za>
2019-07-03 17:00:35 +0200

    insert device consumption

---
+++ Backend/eishms/src/main/java/com/monotoneid/eishms/communications/controller/DeviceConsumptionEndPointController.java
@@ -0,0 +1,37 @@
+package com.monotoneid.eishms.communications.controller;
+
+import javax.validation.Valid;
+
+import com.monotoneid.eishms.dataPersistence.models.DeviceConsumption;
+import com.monotoneid.eishms.services.databaseManagementSystem.DeviceConsumptionService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin(origins = "*", maxAge = 3600)
+@RestController
+@RequestMapping("/api/device")
+public class DeviceConsumptionEndPointController{
+
+    @Autowired
+   private DeviceConsumptionService deviceConsumptionService;
+
+    /**
+    * GET METHOD
+    * Implements retrieveDeviceConsumption endpoint, that calls the retrieveDeviceConsumption service
+    * @param deviceId
+    * @return a the valid Device
+    */
+    @GetMapping(value = "/consumption",params = {"deviceId"})
+    //@PreAuthorize("hasRole('RESIDENT') or hasRole('ADMIN') or hasRole('GUEST')")
+    public ResponseEntity<DeviceConsumption> retriveDevice(@Valid @RequestParam(value = "deviceId") long deviceId){
+       return deviceConsumptionService.retrieveDeviceConsumptionById(deviceId);
+    }
+
+}
--- Backend/eishms/src/main/java/com/monotoneid/eishms/services/databaseManagementSystem/DeviceConsumptionService.java
+++ Backend/eishms/src/main/java/com/monotoneid/eishms/services/databaseManagementSystem/DeviceConsumptionService.java
@@ -3,8 +3,10 @@ package com.monotoneid.eishms.services.databaseManagementSystem;
 import java.sql.Timestamp;
 import java.util.List;
 
+import com.monotoneid.eishms.dataPersistence.models.Device;
 import com.monotoneid.eishms.dataPersistence.models.DeviceConsumption;
 import com.monotoneid.eishms.dataPersistence.repositories.DeviceConsumptions;
+import com.monotoneid.eishms.dataPersistence.repositories.Devices;
 import com.monotoneid.eishms.exceptions.ResourceNotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,11 +16,26 @@ import org.springframework.stereotype.Service;
 public class DeviceConsumptionService{
     @Autowired
     private DeviceConsumptions deviceConsumptionRepository;
+    @Autowired
+    private Devices devicesRepository;
 
+
+
+    public void insertDeviceConsumption(long referenceDeviceId,  Timestamp newDeviceConsumptionTimestamp, String newDeviceConsumptionState, float newDeviceConsumption){
+        try{
+            Device foundDevice = devicesRepository.findById(referenceDeviceId)
+                    .orElseThrow(() -> new ResourceNotFoundException("Device does not exist"));
+            DeviceConsumption newDeviceToAdd = new DeviceConsumption(newDeviceConsumption, foundDevice, newDeviceConsumptionTimestamp, newDeviceConsumptionState);
+            deviceConsumptionRepository.save(newDeviceToAdd);        
+        }catch (Exception e) {
+            System.out.println("Error: " + e.getMessage() + "!");
+        }
+   }
+        /*
     public List<DeviceConsumption> retrieveAllConsumptions(){
         return deviceConsumptionRepository.findAll();
     }
-    public List<DeviceConsumption> retrieveDeviceConsumptionById(long DeviceToFind){
+    public List<DeviceConsumption> retrieveDeviceConsumptionById(long deviceToFind){
         List<DeviceConsumption> foundDeviceConsumptionList = null;
         try{
              foundDeviceConsumptionList = deviceConsumptionRepository.getDeviceConsumptionofId(DeviceToFind)
@@ -30,7 +47,6 @@ public class DeviceConsumptionService{
         }
     }
 
-public void  addDeviceConsumptionOfDeviceId(long referenceDeviceId,  Timestamp newDeviceConsumptionTimestamp, String newDeviceConsumptionSate, float newdeviceConsumption){
-    deviceConsumptionRepository.insertIntoDeviceConsumption(referenceDeviceId, newDeviceConsumptionTimestamp, newDeviceConsumptionSate, newdeviceConsumption);;
-}
+
+*/
 }
