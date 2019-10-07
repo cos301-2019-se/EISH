1800a1869684affb1312be57a1621b0687b2c4a2
Given <u15135421@tuks.co.za>
2019-10-07 12:54:01 +0200

    Updated the Backend folder

--- Backend_Old/eishms/src/main/java/com/monotoneid/eishms/repository/GuestsRepository.java
+++
@@ -1,9 +0,0 @@
-package com.monotoneid.eishms.repository;
-import com.monotoneid.eishms.model.Guests;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository()
-public interface GuestsRepository extends JpaRepository<Guests,Float> {
-}
--- Backend_Old/eishms/src/main/java/com/monotoneid/eishms/repository/HomeOccupationRepository.java
+++
@@ -1,8 +0,0 @@
-package com.monotoneid.eishms.repository;
-import com.monotoneid.eishms.model.HomeOccupation;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository()
-public interface HomeOccupationRepository extends JpaRepository<HomeOccupation,Float> {
-}
