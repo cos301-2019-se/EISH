fd0531994846776577cf05dad9aa80fba9b75bd9
ntlatlengL <u15016715@tuks.co.za>
2019-10-08 10:27:53 +0200

    test changes

--- Frontend/src/app/services/consumption/consumption.service.spec.ts
+++ Frontend/src/app/services/consumption/consumption.service.spec.ts
@@ -1,5 +1,5 @@
-import { TestBed } from '@angular/core/testing';
 
+import { TestBed, inject, getTestBed, fakeAsync, tick } from '@angular/core/testing';
 import { ConsumptionService } from './consumption.service';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { BrowserDynamicTestingModule, platformBrowserDynamicTesting } from '@angular/platform-browser-dynamic/testing';
@@ -21,27 +21,6 @@ describe('ConsumptionService', () => {
   });
 
   
-  it('get today\'s total consumption', fakeAsync(inject(
-      [HttpTestingController, ConsumptionService],
-      (httpMock: HttpTestingController, consumptionService: ConsumptionService) => {
-        const consumption = [
-          homeConsumptionTimeStamp: "2019-07-11 20:47:11",
-          homeConsumption: 10.0
-      ];
-    
-      consumptionService.getDayTotalConsumption(
-        actualDevices => {
-          expect(actualDevices.length).toBe(2);
-          expect(actualDevices[0].deviceId).toEqual(1);
-        }
-      );
-
-      tick();
-      const req = httpMock.expectOne('http://eishms.ddns.net:8080/api/home/consumption/day');
-      expect(req.request.method).toEqual('GET');
-
-      req.flush(mockDevices);
-      httpMock.verify();
-      })));
+ 
 
 });
